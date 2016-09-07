cd3762a845e8531fa94d56b490f0fd4b3c9d296f
beyond_ml@hotmail.com <beyond-ml@hotmail.com>
2016-09-06 18:41:16 -0700

    half done of BinaryTreeRightSideView

---
+++ LeetCode/BinaryTree/BinaryTreeRightSideView.cs
@@ -0,0 +1,43 @@
+﻿/*
+Given a binary tree, imagine yourself standing on the right side of it, return the values of the nodes you can see ordered from top to bottom.
+For example:
+Given the following binary tree,
+   1            <---
+ /   \
+2     3         <---
+ \     \
+  5     4       <---
+You should return [1, 3, 4]. 
+Credits:
+Special thanks to @amrsaqr for adding this problem and creating all test cases.
+
+From <https://leetcode.com/problems/binary-tree-right-side-view/> 
+ */
+using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace InterviewProject
+{
+    public class BinaryTreeRightSideView
+    {
+        public IList<int> RightSideView(TreeNode root) 
+        {
+            IList<int> rightSideNodesList = new List<int>();
+            
+            
+            
+            return rightSideNodesList;
+        }
+    }
+    
+    [TestFixture]
+    public class UTBinaryTreeRightSideView
+    {
+        [Test]
+        public void TestMethod()
+        {
+            // TODO: Add your test.
+        }
+    }
+}
