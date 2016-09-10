04e1089349f0f0c30e221cd9d204297f421ebbe0
beyond_ml@hotmail.com <beyond-ml@hotmail.com>
2016-09-10 15:50:06 -0700

    +kthLargestelementInAnArray, +MinimumsizeSubarraySum

---
+++ LeetCode/Array/KthLargestElementInAnArray.cs
@@ -0,0 +1,40 @@
+﻿/*
+Find the kth largest element in an unsorted array. Note that it is the kth 
+largest element in the sorted order, not the kth distinct element.
+
+For example,
+Given [3,2,1,5,6,4] and k = 2, return 5.
+
+Note: 
+You may assume k is always valid, 1 ≤ k ≤ array's length.
+
+https://leetcode.com/problems/kth-largest-element-in-an-array/
+ */
+using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace LeetCode
+{
+    public class KthLargestElementInAnArray
+    {
+        public static int FindKthLargest(int[] nums, int k) 
+        {
+            List<int> list = new List<int>(nums);
+            list.Sort();
+            return list[nums.Length - k];
+        }
+    }
+    
+    [TestFixture]
+    public class UTKthLargestElementInAnArray
+    {
+        [Test]
+        public void TestMethod()
+        {
+            int[] input = {3,2,1,5,6,4};
+            var n = KthLargestElementInAnArray.FindKthLargest(input, 2);
+            Assert.IsTrue(n == 5);
+        }
+    }
+}
---
+++ LeetCode/Array/MinimumSizeSubarraySum.cs
@@ -0,0 +1,136 @@
+﻿/*
+209. Minimum Size Subarray Sum  QuestionEditorial Solution  My Submissions
+
+Given an array of n positive integers and a positive integer s, find the minimal length of a subarray
+of which the sum ≥ s. If there isn't one, return 0 instead.
+
+For example, given the array [2,3,1,2,4,3] and s = 7,
+the subarray [4,3] has the minimal length under the problem constraint.
+
+https://leetcode.com/problems/minimum-size-subarray-sum/
+ 
+ */
+using System;
+using NUnit.Framework;
+
+namespace LeetCode
+{
+    public class MinimumSizeSubarraySum
+    {
+        /// <summary>
+        /// return the min subarray length which total is same as target
+        /// </summary>
+        /// <param name="s"></param>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public static int MinSubArrayLenSumAsTarget(int s, int[] nums)
+        {
+            int result = 0;
+            
+            if (nums.Length == 0)
+            {
+                return result;
+            }
+            else if (nums[0] == s)
+            {
+                return 1;
+            }
+            
+            int currSum = nums[0];
+            int currStart = 0;
+            
+            for (int i = 1; i < nums.Length; i++)
+            {
+                currSum += nums[i];
+                
+                while (7 <= currSum)
+                {
+                    if (7 == currSum)
+                    {
+                        int length = i - currStart + 1;
+                        
+                        if (result == 0)
+                            result = length;
+                        else
+                            result = Math.Min(result, length);
+                    }
+                    
+                    currSum -= nums[currStart];
+                    currStart++;
+                }
+
+            }
+            
+            return result;
+        }
+        
+        public static int MinSubArrayLenSumAboveTarget(int s, int[] nums)
+        {
+            int result = 0;
+            
+            if (nums.Length == 0)
+            {
+                return result;
+            }
+            else if (nums[0] >= s)
+            {
+                return 1;
+            }
+            
+            int currSum = nums[0];
+            int currStart = 0;
+            
+            for (int i = 1; i < nums.Length; i++)
+            {
+                currSum += nums[i];
+                
+                while (s <= currSum)
+                {
+                    int length = i - currStart + 1;
+                    
+                    if (result == 0)
+                        result = length;
+                    else
+                        result = Math.Min(result, length);
+                    
+                    currSum -= nums[currStart];
+                    currStart++;
+
+                }
+            }
+            
+            return result;
+        }
+    }
+    
+    [TestFixture]
+    public class UTMinSubArrayLenSumAsTarget
+    {
+        [Test]
+        public void TestMethod()
+        {
+            int[] array = {2,3,1,2,4,3};
+            
+            Assert.IsTrue(2 == MinimumSizeSubarraySum.MinSubArrayLenSumAsTarget(7, array));
+        }
+    }
+    [TestFixture]
+    public class UTMinimumSizeSubarraySumAvoveTarget
+    {
+        [Test]
+        public void TestMethod1()
+        {    
+            int[] array = {2,3,1,2,4,3};
+            var len = MinimumSizeSubarraySum.MinSubArrayLenSumAboveTarget(7, array);
+            Assert.IsTrue(2 == len, "result: {0}", len);
+        }
+        
+        [Test]
+        public void TestMethod2()
+        {
+            int[] array = {1,2,3,4,5};
+            var len = MinimumSizeSubarraySum.MinSubArrayLenSumAboveTarget(11, array);
+            Assert.IsTrue(3 == len, "result: {0}", len);
+        }
+    }
+}
