bd4eabeb6b16398d047cd42a07ff14562ee502d8
beyond_ml <beyond_ml@hotmail.com>
2016-09-10 23:18:27 -0700

    +intersectionoftwoarray +productofarrayexceptself

---
+++ LeetCode/Array/IntersectionofTwoArrays.cs
@@ -0,0 +1,54 @@
+﻿/*
+  Intersection of Two Arrays  QuestionEditorial Solution  My Submissions
+
+Example:
+Given nums1 = [1, 2, 2, 1], nums2 = [2, 2], return [2].
+
+Note:
+Each element in the result must be unique.
+The result can be in any order.
+Subscribe to see which companies ask
+https://leetcode.com/problems/intersection-of-two-arrays/
+*/
+using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+    public class IntersectionofTwoArrays
+    {
+        public static int[] Intersection(int[] nums1, int[] nums2)
+        {
+            List<int> list = new List<int>(nums1);
+            HashSet<int> result = new HashSet<int>();
+            
+            foreach(int i in nums2)
+            {
+                if (list.Contains(i))
+                    result.Add(i);
+            }
+
+            return result.ToArray();
+        }
+    }
+
+    [TestFixture]
+    public class UTIntersectionofTwoArrays
+    {
+        [Test]
+        public void TestIntersectionOfTwoArrays()
+        {
+            int[] arr1 = { 1, 2, 2, 1 };
+            int[] arr2 = { 2, 2 };
+
+            var result = IntersectionofTwoArrays.Intersection(arr1, arr2);
+
+            Assert.IsTrue(result.Length == 1);
+            Assert.IsTrue(result[0] == 2);
+        }
+    }
+}
---
+++ LeetCode/Array/ProductofArrayExceptSelf.cs
@@ -0,0 +1,64 @@
+﻿/*
+ 238. Product of Array Except Self  QuestionEditorial Solution  My Submissions
+Total Accepted: 61017
+Total Submissions: 135166
+Difficulty: Medium
+Given an array of n integers where n > 1, nums, return an array output such that output[i] is equal to the product of all the elements of nums except nums[i].
+
+Solve it without division and in O(n).
+
+For example, given [1,2,3,4], return [24,12,8,6].
+
+Follow up:
+Could you solve it with constant space complexity? (Note: The output array does not count as extra space for the purpose of space complexity analysis.)
+https://leetcode.com/problems/product-of-array-except-self/
+ */
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode
+{
+    class ProductofArrayExceptSelf
+    {
+        public static int[] ProductExceptSelf(int[] nums)
+        {
+            int[] result = new int[nums.Length];
+            result[nums.Length - 1] = 1;
+
+            for (int i = nums.Length -2 ; i >= 0 ; i--)
+            {
+                result[i] = nums[i + 1] * result[i + 1];
+            }
+
+            int left = nums[0];
+            for (int i = 1; i <= nums.Length -1; i++)
+            {
+                result[i] = left * result[i];
+                left = left * nums[i];
+            }
+
+            return result;
+        }
+    }
+
+    [TestFixture]
+    public class UTProductofArrayExceptSelf
+    {
+        [Test]
+        public void TestProductOfArrayExceptSelf1()
+        {
+            int[] input = new int[] { 1, 2, 3, 4 };
+
+            var result = ProductofArrayExceptSelf.ProductExceptSelf(input);
+
+            Assert.IsTrue(result[0] == 24);
+            Assert.IsTrue(result[1] == 12);
+            Assert.IsTrue(result[2] == 8);
+            Assert.IsTrue(result[3] == 6);
+        }
+    }
+
+}
