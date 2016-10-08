9006959eb8158d7fc782625955fc946ca88f3904
beyond_ml <beyond_ml@hotmail.com>
2016-10-07 23:24:21 -0700

    +3Sum +LongestCommonPrefix +IntegerToRoman +RomanToInteger +PalindromeNumber

---
+++ LeetCode/Array/3Sum.cs
@@ -0,0 +1,108 @@
+﻿/*
+ Given an array S of n integers, are there elements a, b, c in S such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
+
+Note: The solution set must not contain duplicate triplets.
+For example, given array S = [-1, 0, 1, 2, -1, -4],
+
+A solution set is:
+[
+  [-1, 0, 1],
+  [-1, -1, 2]
+]
+*/
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Array
+{
+    class _3Sum
+    {
+        public IList<IList<int>> ThreeSum(int[] nums)
+        {
+            IList<IList<int>> results = new List<IList<int>>();
+
+            List<int> numbers = new List<int>(nums);
+            numbers.Sort();
+
+            for (int i = numbers.Count - 1; i > 1 ; i--)
+            {
+                int target = numbers[i] * -1;
+                
+                if (i < numbers.Count - 1 && target == numbers[i+1] * -1)
+                {
+                    continue;
+                }
+
+                target = numbers[i] * -1;
+
+                int p = 0, q = i - 1;
+
+                while (p < q)
+                {
+                    int result = numbers[p] + numbers[q];
+                    if (result == target)
+                    {
+                        List<int> r = new List<int>(new int[] { numbers[p], numbers[q], numbers[i]});
+
+                        results.Add(r);
+
+                        while (++p < i - 1 && numbers[p] == numbers[p - 1]) ;
+
+                        while (--q > 0 && numbers[q] == numbers[q + 1]) ;
+                    }
+                    else if (result < target)
+                    {
+                        while (++p < i - 1 && numbers[p] == numbers[p - 1]) ;
+                    }
+                    else
+                    {
+                        while (--q > 0 && numbers[q] == numbers[q + 1]) ;
+                    }
+                }
+            }
+
+            return results;
+        }
+    }
+
+
+    [TestFixture]
+    class UT_3Sum
+    {
+        [Test]
+        public void TestThreeSum()
+        {
+            int[] input = { -1, 0, 1, 2, -1, -4 };
+            _3Sum sum = new _3Sum();
+
+            var result = sum.ThreeSum(input);
+
+            Assert.IsTrue(result.Count == 2);
+        }
+
+        [Test]
+        public void TestThreeSum1()
+        {
+            int[] input = { 0,0,0,0 };
+            _3Sum sum = new _3Sum();
+
+            var result = sum.ThreeSum(input);
+
+            Assert.IsTrue(result.Count == 1);
+        }
+
+        [Test]
+        public void TestThreeSum2()
+        {
+            int[] input = {-4,-2,1,-5,-4,-4,4,-2,0,4,0,-2,3,1,-5,0};
+            _3Sum sum = new _3Sum();
+
+            var result = sum.ThreeSum(input);
+
+            Assert.IsTrue(result.Count == 6);
+        }
+    }
+}
---
+++ LeetCode/Number/IntegerToRoman.cs
@@ -0,0 +1,81 @@
+﻿/*
+ * [LeetCode] Integer to Roman
+Given a roman numeral, convert it to an integer.
+Input is guaranteed to be within the range from 1 to 3999.
+
+思路：
+
+这题需要一些背景知识，首先要知道罗马数字是怎么表示的：
+
+http://en.wikipedia.org/wiki/Roman_numerals
+
+I: 1
+V: 5
+X: 10
+L: 50
+C: 100
+D: 500
+M: 1000
+
+字母可以重复，但不超过三次，当需要超过三次时，用与下一位的组合表示：
+I: 1, II: 2, III: 3, IV: 4
+C: 100, CC: 200, CCC: 300, CD: 400
+
+举例： s = 3978
+    3978/1000 = 3: MMM
+    978>(1000-100), 998/900 = 1: CM
+    78<(100-10), 78/50 = 1 :L
+    28<(50-10), 28/10 = XX
+    8<(100-1), 8/5 = 1: V
+    3<(5-1), 3/1 = 3: III
+    ret = MMMCMLXXVIII
+
+所以可以将单个罗马字符扩展成组合形式，来避免需要额外处理类似IX这种特殊情况。
+I: 1
+IV: 4
+V: 5
+IX: 9
+X: 10
+XL: 40
+L: 50
+XC: 90
+C: 100
+CD: 400
+D: 500
+CM: 900
+M: 1000
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Number
+{
+    class GetIntegerToRoman
+    {
+        public string IntToRoman(int num)
+        {
+            string[] dict = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+            int[] val = { 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+
+            string result = string.Empty;
+
+            for (int i = 0; i < dict.Length; i++)
+            {
+                if (num >= val[i])
+                {
+                    int c = num / val[i];
+                    num %= val[i];
+
+                    for (int j = 0; j < c; j++)
+                    {
+                        result += dict[i];
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
