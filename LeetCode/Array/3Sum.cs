fd986a667080798b8072586597bf153997f93663
beyond_ml@hotmail.com <beyond-ml@hotmail.com>
2016-10-10 10:09:41 -0700

    fix the namespace issue

--- LeetCode/Array/3Sum.cs
+++ LeetCode/Array/3Sum.cs
@@ -16,7 +16,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace LeetCode.Array
+namespace LeetCode
 {
     class _3Sum
     {
--- LeetCode/Array/AddTwoNumbers.cs
+++ LeetCode/Array/AddTwoNumbers.cs
@@ -11,7 +11,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace LeetCode.Array
+namespace LeetCode
 {
     class AddTwoNumbers
     {
