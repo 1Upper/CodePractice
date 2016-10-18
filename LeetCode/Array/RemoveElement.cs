ba2effbfbf63635ebef33322442d6b32a170253c
beyond_ml <beyond_ml@hotmail.com>
2016-10-18 00:22:27 -0700

    + RemoveElement + DivideTwoIntegers

---
+++ LeetCode/Array/RemoveElement.cs
@@ -0,0 +1,60 @@
+﻿/*
+ Remove Element   QuestionEditorial Solution  My Submissions
+Total Accepted: 151099
+Total Submissions: 418789
+Difficulty: Easy
+Contributors: Admin
+Given an array and a value, remove all instances of that value in place and return the new length.
+
+Do not allocate extra space for another array, you must do this in place with constant memory.
+
+The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+
+Example:
+Given input array nums = [3,2,2,3], val = 3
+
+Your function should return length = 2, with the first two elements of nums being 2.
+
+Hint:
+
+Try two pointers.Show More Hint 
+
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Array
+{
+    class RemoveElement
+    {
+        public int GetRemoveElement(int[] nums, int val)
+        {
+            if (nums.Length < 1)
+                return 0;
+
+            int p1 = 0;
+            int p2 = 0;
+
+            while (p2 < nums.Length)
+            {
+                if (nums[p2] == val)
+                {
+                    p2 ++;
+                }
+                else if (p1 == p2)
+                {
+                    p2 ++;
+                    p1 ++;
+                }
+                else
+                {
+                    nums[p1++] = nums[p2++];
+                }
+            }
+
+            return p1;
+        }
+    }
+}
---
+++ LeetCode/Number/DivideTwoIntegers.cs
@@ -0,0 +1,67 @@
+﻿/*
+ LeetCode – Divide Two Integers (Java)
+ 
+Divide two integers without using multiplication, division and mod operator. If it is overflow, return MAX_INT.
+
+Analysis
+
+This problem can be solved based on the fact that any number can be converted to the format of the following:
+
+num=a_0*2^0+a_1*2^1+a_2*2^2+...+a_n*2^n
+The time complexity is O(logn).
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace LeetCode.Number
+{
+    class DivideTwoIntegers
+    {
+        public int Divide(int dividend, int divisor)
+        {
+            if (divisor == 0) return int.MinValue;
+            if (divisor == -1 && dividend == int.MinValue) return int.MaxValue;
+
+            Int64 pDividend = Math.Abs((Int64)dividend);
+            Int64 pDivisor = Math.Abs((Int64)divisor);
+
+            int result = 0;
+
+            while (pDividend >= pDivisor)
+            {
+                int numShift = 0;
+                while (pDividend >= (pDivisor << numShift))
+                {
+                    numShift++;
+                }
+
+                result += 1 << (numShift - 1);
+                pDividend -= pDivisor << (numShift - 1);
+            }
+
+            if ((dividend >= 0 && divisor >= 0) || (dividend < 0 && divisor < 0))
+            {
+                return result;
+            }
+            else
+            {
+                return -result;
+            }
+        }
+    }
+
+    [TestFixture]
+    class TestDivideTwoIntegers
+    {
+        [Test]
+        public void UTDivideTwoIntegers()
+        {
+            DivideTwoIntegers d = new DivideTwoIntegers();
+            int result = d.Divide(1, -1);
+            Assert.IsTrue(result == -1);
+        }
+    }
+}
